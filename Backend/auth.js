b2165d5462f62e45046b5f7854e8be909a2c7b7c
bsaikiran618 <bsaikiran618@gmail.com>
2021-03-27 12:41:34 +0530

    Basic Database connection fone

--- Backend/auth.js
+++ Backend/auth.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const router = express.Router();
-
+const {Client} = require("pg");
+const dbObject = require("./databaseObject.js");
 //Authentication endpoints
 
 router.post("/login", (req, res)=>{
-	
+
+
+	console.log(JSON.stringify(dbObject)); //DEBUG
+
+	const client = new Client(dbObject);
+	client.connect();
+
+	client.query("SELECT NOW()")
+	.then((res)=>{
+		console.log(res);
+		client.end();
+	})
+	.catch(error=>{
+		console.log(error);
+		client.end()
+	})
+
 });
 
 router.post("/signup", (req, res)=>{
